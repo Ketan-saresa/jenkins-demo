1eb5dab181dedb84093b13531e0ebfa4d14a7194
Jayesh <jgv1593@gmail.com>
2016-12-02 17:36:34 +0530

    API call

--- app/app.config.js
+++ app/app.config.js
@@ -1,5 +1,11 @@
 var app = angular.module("jetkinsDemo", [])
-    app.controller('appCtrl',function($scope){
-        $scope.data="ABCD";
-        console.log($scope.data);
+    app.controller('appCtrl',function($scope,$http){
+        
+        $http.get('http://45.55.220.81:3333/api/test')
+        .success(function (data) {
+            console.log(data);
+        })
+        .error(function (data) {
+            console.log(data);
+        });
     })
